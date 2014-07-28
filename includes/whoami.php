fef2d7bdd7f20c17f8cbe246675ca65fd0ca342a
atomicjam <atomicjam@gmail.com>
2014-07-28 14:47:32 +0100

    updated sql dump and removed old files

--- includes/whoami.php
+++
@@ -1,8 +0,0 @@
-<?php
-//set cookie to username so engineer forms know who you are 
-// for testing this is a static engineer id in this case 8 but in production would use ntml or login to set id
-setcookie("engineerid", "8", time()+3600, "/");
-
-// need to write code to set id using ntml or email login form.
-
-?>
