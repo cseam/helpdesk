1adde6b2a86a7629fd2b84c1836d220be2f5f2af
atomicjam <atomicjam@gmail.com>
2014-07-31 16:18:07 +0100

    dynamic graph from db

--- graphs.php
+++ graphs.php
@@ -1,9 +1,60 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 	<?php
 	// load functions
-	include 'includes/functions.php';
+	include 'includes/functions.php';	
+	// sql
+	
+	$sqlstr = "SELECT closeengineerid, DATE_FORMAT(closed, '%a')AS DAY_OF_WEEK FROM calls WHERE closeengineerid = '".$_SESSION['engineerId']."' AND closed >= DATE_SUB(CURDATE(),INTERVAL 7 DAY)"; 
+		$engineermon = 0;
+		$engineertue = 0;
+		$engineerwed = 0;
+		$engineerthu = 0;
+		$engineerfri = 0;
+		$engineersat = 0;
+		$engineersun = 0;
+		
+	// Run Query
+		$resultengineer = mysqli_query($db, $sqlstr); 
+		while($stats = mysqli_fetch_array($resultengineer))  { 	
+			if ($stats['DAY_OF_WEEK'] == "Mon") { ++$engineermon; };
+			if ($stats['DAY_OF_WEEK'] == "Tue") { ++$engineertue; };
+			if ($stats['DAY_OF_WEEK'] == "Wed") { ++$engineerwed; };
+			if ($stats['DAY_OF_WEEK'] == "Thu") { ++$engineerthu; };
+			if ($stats['DAY_OF_WEEK'] == "Fri") { ++$engineerfri; };
+			if ($stats['DAY_OF_WEEK'] == "Sat") { ++$engineersat; };
+			if ($stats['DAY_OF_WEEK'] == "Sun") { ++$engineersun; };
+		}
+
+	$sqlstrall = "SELECT closeengineerid, DATE_FORMAT(closed, '%a') AS DAY_OF_WEEK FROM calls WHERE closed >= DATE_SUB(CURDATE(),INTERVAL 7 DAY)"; 
+
+		$allmon = 0;
+		$alltue = 0;
+		$allwed = 0;
+		$allthu = 0;
+		$allfri = 0;
+		$allsat = 0;
+		$allsun = 0;
+
+		$resultall = mysqli_query($db, $sqlstrall); 
+		while($stats = mysqli_fetch_array($resultall))  { 
+		
+			
+			if ($stats['DAY_OF_WEEK'] == "Mon") { ++$allmon; };
+			if ($stats['DAY_OF_WEEK'] == "Tue") { ++$alltue; };
+			if ($stats['DAY_OF_WEEK'] == "Wed") { ++$allwed; };
+			if ($stats['DAY_OF_WEEK'] == "Thu") { ++$allthu; };
+			if ($stats['DAY_OF_WEEK'] == "Fri") { ++$allfri; };
+			if ($stats['DAY_OF_WEEK'] == "Sat") { ++$allsat; };
+			if ($stats['DAY_OF_WEEK'] == "Sun") { ++$allsun; };
+		}
+
+	
 	?>
+	
+	
+	
 	<head>
 		<title><?=$codename;?></title>
 		<link rel="shortcut icon" href="clcfavicon.ico" type="image/x-icon" />
@@ -39,14 +90,14 @@
       google.setOnLoadCallback(drawChart2);
       function drawChart2() {
         var data = google.visualization.arrayToDataTable([
-          ['Day', 'All', 'Your'],
-          ['Sun',  14,      0],
-          ['Mon',  88,      14],
-          ['Tue',  70,      22],
-          ['Wed',  66,       11],
-          ['Thu',  108,      26],
-          ['Fri',  92,      14],
-          ['Sat',  24,      0]
+          ['Day', 'All', 'Me'],
+          ['Sun',  <?=$allsun?>,      <?=$engineersun?>],
+          ['Mon',  <?=$allmon?>,      <?=$engineermon?>],
+          ['Tue',  <?=$alltue?>,      <?=$engineertue?>],
+          ['Wed',  <?=$allwed?>,      <?=$engineerwed?>],
+          ['Thu',  <?=$allthu?>,      <?=$engineerthu?>],
+          ['Fri',  <?=$allfri?>,      <?=$engineerfri?>],
+          ['Sat',  <?=$allsat?>,      <?=$engineersat?>]
         ]);
 
         var options = {
@@ -54,24 +105,21 @@
           legend: { position: 'right' },
           colors: ['#577d6a','#CCCCCC'],
           pointSize: 4,
-          vAxis: {gridlines: { count: 3 },textPosition: 'none'},
-          chartArea: {'width': 'auto', 'height': '70%', 'left' : 0,},
+          vAxis: {gridlines: { count: 4 },},
+          chartArea: {'width': 'auto', 'height': '70%',},
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('linechart'));
         chart.draw(data, options);
         }
-    </script>
-		
-		
-		
+    </script>		
 	</head>
 	<body>
 	<div class="section">
 	<h2>Graphs</h2>
 	<h3>Performance</h3>
 	<div id="piechart" style="width: 20%; float: left; -webkit-box-sizing: border-box;"></div>
-	<div id="linechart" style="width: 40%; float: left; -webkit-box-sizing: border-box;"></div>
+	<div id="linechart" style="width: 40%; float: left; -webkit-box-sizing: border-box;"></div>	
 	<ul style="clear: left;">
 		<li><a href="index.php"><?=$codename;?> Home</a></li>
 	</ul>	
