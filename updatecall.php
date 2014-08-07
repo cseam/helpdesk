985c7bbd419d02275be70d235d1906c264b57bf4
atomicjam <atomicjam@gmail.com>
2014-08-07 10:46:48 +0100

    updated visuals on update & close call

--- updatecall.php
+++ updatecall.php
@@ -2,10 +2,11 @@
 <!DOCTYPE html>
 <html lang="en">
 	<?php
+	// load functions
 	include 'includes/functions.php';
 	?>
 	<head>
-		<title><?=$codename;?> - All Calls</title>
+		<title><?=$codename;?> - Update Calls</title>
 		<link rel="shortcut icon" href="clcfavicon.ico" type="image/x-icon" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" /> 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -15,6 +16,29 @@
 	</head>
 	<body>
 	<div class="section">
+<div id="branding">
+		<a href="add.php">Add Call</a><br/>
+		<a href="engineerview.php">Engineer view</a><br/>
+	</div>
+	
+	<div id="leftpage">
+	<div id="stats">
+		<h3>Information</h3>
+		<p>Welcome to helpdesk <?=$_SESSION['sAMAccountName'];?>, please use the form to log calls for engineers, once your call has been logged you will receive email feedback on your issue, you can also return here at any time to see the status of your calls.</p>
+		<p>Please remember the more information you can provide the quicker the engineer can fix your problem, for example when your printer is out of ink please include as much information as possible, printer model, colour of ink cartridge, room the printer is in. etc.. this saves the engineer asking these questions at a later point and slowing down the process.</p>
+	</div>
+	<div id="calllist">
+		<h3>Your Helpdesks</h3>
+		<?php include 'includes/yourcalls.php'; ?>
+	</div>
+	</div>
+	<div id="rightpage">
+		<div id="addcall">
+			<div id="ajax">
+	
+
+
+
 
 <?php if ($_SERVER['REQUEST_METHOD']== "POST") { 
 
@@ -30,8 +54,9 @@ if (isset($_POST['close'])) {
        $sqlstr .= "WHERE callid='" . mysqli_real_escape_string($db,$_POST['id']) . "'";
        // Run query
        mysqli_query($db, $sqlstr); 
-       echo "<h2>Call Updated & Closed #" . $_POST['id'] . "</h2>";
-       echo "<p>SQL: " . $sqlstr . "</p>";
+       echo "<h2>Updated & Closed</h2>";
+       echo "<p>Call #" . $_POST['id'] . " has been updated and closed, all stake holders have been emailed.</p>";
+       echo "<p><a href='/'>Home</a></p>";
     }
 if (isset($_POST['update'])) {
 		// update call
@@ -44,16 +69,16 @@ if (isset($_POST['update'])) {
 		$sqlupdatestr .= "WHERE callid='" . mysqli_real_escape_string($db,$_POST['id']) . "'";
 		// Run query
 		mysqli_query($db, $sqlupdatestr);
-		echo "<h2>Call updated #". $_POST['id'] ."</h2>";
-        echo "<p>SQL: " . $sqlupdatestr . "</p>";
+		echo "<h2>Call updated</h2>";
+        echo "<p>Call #" . $_POST['id'] . " has been updated, all stake holders have been emailed.</p>";
+        echo "<p><a href='/'>Home</a></p>";
 }
 
 } ?>
-
-	<ul>
-		<li><a href="index.php"><?=$codename;?> Home</a></li>
-	</ul>
 </div>
+		</div>
+	</div>
+	</div>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>	
 	<script src="javascript/jquery.js" type="text/javascript"></script>
 	</body>
