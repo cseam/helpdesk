e47b5f6b724c557df1fdfebaf8360dc0e30d1877
atomicjam <atomicjam@gmail.com>
2014-08-19 15:34:07 +0100

    added emerging issues

--- includes/managerreport7.php
+++ includes/managerreport7.php
@@ -1,1 +1,35 @@
-<p>Pitchfork chillwave tousled quinoa, selfies Portland plaid raw denim. Paleo quinoa Williamsburg forage, synth Thundercats retro you probably haven't heard of them leggings before they sold out deep v vegan banjo. Seitan locavore Intelligentsia art party High Life, 3 wolf moon +1 lomo. Austin 90's selfies 3 wolf moon. Wayfarers cliche food truck distillery seitan raw denim. Normcore Blue Bottle Odd Future flexitarian roof party tattooed, Thundercats actually hashtag squid jean shorts readymade brunch. PBR&B McSweeney's polaroid ennui, biodiesel before they sold out selvage pug Odd Future authentic direct trade semiotics.</p>
+<?php 
+	include_once('../includes/functions.php');
+?>
+<p>Issues logged by area in last 7 days, high numbers may indicate a problem in that house</p>
+<table>
+<tr>
+	<th>Location</th>
+	<th>Calls Logged</th>
+</tr>
+<?	
+	$sql ="SELECT COUNT(*) AS Number_of_Calls,c.location AS Location, l.locationName AS Location_Name FROM calls AS c LEFT JOIN location as l ON c.location = l.id WHERE opened >= DATE_SUB(CURDATE(),INTERVAL 7 DAY) AND location != 1 GROUP BY location HAVING COUNT(*)>=3";
+	$result = mysqli_query($db, $sql);
+		while($loop = mysqli_fetch_array($result)) { ?>
+<tr>
+	<td><?=$loop['Location_Name']?></td>
+	<td><?=$loop['Number_of_Calls']?></td>
+</tr>
+<? } ?>
+</table>
+<p>Reoccurring string logged for room location, high numbers may indicate a problem in that location</p>
+<table>
+<tr>
+	<th>Room</th>
+	<th>Calls Logged</th>
+</tr>
+<?	
+	$sql ="SELECT COUNT(*) AS Number_of_Calls, room AS Room FROM calls WHERE opened >= DATE_SUB(CURDATE(),INTERVAL 7 DAY) GROUP BY room HAVING COUNT(*) >=3";
+	$result = mysqli_query($db, $sql);
+		while($loop = mysqli_fetch_array($result)) { ?>
+<tr>
+	<td><?=$loop['Room']?></td>
+	<td><?=$loop['Number_of_Calls']?></td>
+</tr>
+<? } ?>
+</table>
