4195df24c39948e05084a8177bf54716a649a9d9
atomicjam <atomicjam@gmail.com>
2014-12-09 11:47:47 +0000

    change control history added

--- includes/managerreport-changehistory.php
+++ includes/managerreport-changehistory.php
@@ -3,8 +3,28 @@
 	// load functions
 	include_once '../includes/functions.php';
 ?>
-<h3>Change Control History</h3>
-<div id="poststatus">
-	<? print_r($_POST); ?>
-</div>
-
+<table>
+	<thead>
+		<tr>
+			<th>Who</th>
+			<th>When</th>
+			<th>Where</th>
+			<th>Description</th>
+			<th>Tags</th>
+		</tr>
+	</thead>
+	<tbody>
+<?
+$sqlstr = mysqli_query($db, "SELECT * FROM changecontrol INNER JOIN engineers ON changecontrol.engineersid=engineers.idengineers ;");
+				while($results = mysqli_fetch_array($sqlstr)) {
+						echo "<tr>";
+						echo "<td>" . $results['engineerName'] . "</td>";
+						echo "<td>" . date("d/m h:s", strtotime($results['stamp'])) . "</td>";
+						echo "<td>" . $results['server'] . "</td>";
+						echo "<td>" . $results['changemade'] . "</td>";
+						echo "<td>" . $results['tags'] . "</td>";
+						echo "</tr>";
+};
+?>
+	</tbody>
+</table>
