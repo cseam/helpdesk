e769da9fa911cee4437ba7381f9bdf14aee37843
atomicjam <atomicjam@gmail.com>
2015-01-23 12:18:03 +0000

    visual tweak

--- add.php
+++ add.php
@@ -29,7 +29,7 @@
 	<div id="stats">
 		<h3>Information</h3>
 		<p>Welcome to helpdesk <?=$_SESSION['sAMAccountName'];?>, please use the form to log calls for engineers, once your call has been logged you will receive email feedback on your issue, you can also return here at any time to see the status of your calls.</p>
-		<p>Please remember the more information you can provide the quicker the engineer can fix your problem, for example when your printer is out of ink please include as much information as possible, printer model, colour of ink cartridge, room the printer is in. etc.. this saves the engineer asking these questions at a later point.</p>
+		<p class="note">Remember the more information you provide the quicker the engineer can fix your problem. For example, your printer is out of ink please include, printer model, colour of ink cartridge, room the printer is in. etc.</p>
 	</div>
 	<div id="calllist">
 		<h3>Your Helpdesks</h3>
