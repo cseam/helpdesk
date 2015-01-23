78e82daae1e1f98a57b5db70e09bfe18c7bc29cb
atomicjam <atomicjam@gmail.com>
2015-01-23 10:34:37 +0000

    tidy up

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
 	<?php
 	// load functions
 	include_once 'includes/functions.php';
-	// check authentication 
-	if (empty($_SESSION['sAMAccountName'])) { prompt_auth($_SERVER['REQUEST_URI']); } else 
-	
-	{ 
+	// check authentication
+	if (empty($_SESSION['sAMAccountName'])) { prompt_auth($_SERVER['REQUEST_URI']); } else
+	{
 	// forward to correct page depending on user
 		if ($_SESSION['engineerLevel'] == "2") {
 			// forward to manager page
@@ -22,10 +21,6 @@
 	    };
 	};
 	?>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>	
-	<script src="javascript/jquery.js" type="text/javascript"></script>
 	</head>
-	<body>
-	.
-	</body>
+	<body></body>
 </html>
