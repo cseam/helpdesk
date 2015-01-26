bc6d2f58b16aa7d8aad25d122839d5630d9ce38c
atomicjam <atomicjam@gmail.com>
2015-01-26 14:35:56 +0000

    put header into inc

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!-- header inc -->
+<title><?=$codename;?></title>
+<link rel="shortcut icon" href="clcfavicon.ico" type="image/x-icon" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="refresh" content="600" />
+<meta name="robots" content="nofollow" />
+<link rel="stylesheet" type="text/css" href="/css/reset.css" />
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+<script src="/javascript/global_jquery.js" type="text/javascript"></script>
