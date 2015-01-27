bb5d46bdce490a16756cedfab5a4f139ab4b6a8e
atomicjam <atomicjam@gmail.com>
2015-01-27 15:53:43 +0000

    added google analytics to header inc

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,3 @@
-<!-- header inc -->
 <title><?=$codename;?></title>
 <link rel="shortcut icon" href="clcfavicon.ico" type="image/x-icon" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -10,3 +9,12 @@
 <link rel="stylesheet" type="text/css" href="/css/print.css" media="print"/>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
 <script src="/javascript/global_jquery.js" type="text/javascript"></script>
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-59019267-1', 'auto');
+  ga('send', 'pageview');
+</script>
