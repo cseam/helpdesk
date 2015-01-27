72071cdeb8b94ddb2a500756c4b3dca4e1ff29a7
atomicjam <atomicjam@gmail.com>
2015-01-27 15:48:56 +0000

    updated header inc

--- login/logout.php
+++ login/logout.php
@@ -2,7 +2,7 @@
 	// start sessions
 	session_start();
 	// load functions
-	include_once('../includes/functions.php');
+	include_once($_SERVER['DOCUMENT_ROOT'] . '/includes/functions.php');
 	//Check if enginner logging out
 	if ($_SESSION['engineerId'] !== null) {
 		// Update db enginner status out
