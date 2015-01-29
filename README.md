1e11c60ab6e5df01cec78b0cff187cee57c03973
atomicjam <atomicjam@gmail.com>
2015-01-29 15:05:37 +0000

    awaiting invoice adds values to db

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-helpdesk v 0.2.1
+helpdesk v 4.1.0
 ========
 
-Internal helpdesk, a ticket based system for employees to report various issues on site, to specified departments for action.
+Internal helpdesk to replace old asp helpdesk from 2004, system needs to take existing features and add a ticket based system for employees to report various issues on site, to specified departments for action.
 
 PHP driven, but intention to move to NodeJS in future, responsive designed to be portable for enginneers and quick for users to log issues.
+
+system to be managed and maintained by individual departments and IT to maintain codebase.
--- includes/managerreport-awaitinginvoice.php
+++ includes/managerreport-awaitinginvoice.php
@@ -3,16 +3,23 @@
 	// load functions
 	include_once($_SERVER['DOCUMENT_ROOT'] . '/includes/functions.php');
 ?>
+<?php if ($_SERVER['REQUEST_METHOD']== "POST" & $_POST['toggle'] == TRUE) { ?>
+<h2>Awaiting Invoice</h2>
+<?
+	// update timestamp with changes	
+	$sqlstr = "UPDATE calls SET invoicedate='".date("c")."' WHERE callid='" . $_POST['id'] . "';";
+	$result = mysqli_query($db, $sqlstr);
+ } ?>
 <div id="ajaxforms">
 	<table>
 		<thead>
 			<tr>
 				<td>#</td>
-				<td>Invoice Received</td>
+				<td>Received</td>
+				<td>Date</td>
 				<td>Call Details</td>
 			</tr>
-		</thead>
-		
+		</thead>		
 	<tbody>
 	<?php
 		if ($_SESSION['engineerHelpdesk'] <= '3') {
@@ -26,10 +33,41 @@
 		?>
 		<tr>
 		<td>#<?=$calls['callid'];?></td>
-		<td><input  type="checkbox" name="invoicetoggle"> <?=date("d/m/y");?></td>
+		<td>
+			<div class="switch" style="float:left;">
+			<form action="<?=$_SERVER['PHP_SELF']?>" method="post" class="toggleform" >
+				<input type="hidden" id="toggle" name="toggle" value="TRUE" />
+				<input type="hidden" id="id" name="id" value="<?=$calls['callid'];?>" />
+				<input id="cmn-toggle-<?=$calls['callid']?>" name="cmn-toggle-<?=$calls['callid']?>" class="cmn-toggle cmn-toggle-round" type="checkbox" <? if ($calls['invoicedate'] !== null) { ?>checked="true" <?}?>>
+				<label for="cmn-toggle-<?=$calls['callid']?>"></label>
+			</form>
+		</div>
+		<td><? if ($calls['invoicedate'] !== null) { echo date("d/m/y", strtotime($calls['invoicedate'])); };?></td>
 		<td><?=substr(strip_tags($calls['details']), 0, 65);?>...</td>
 		</tr>
 	<? } ?>
 	</tbody>
 	</table>
 </div>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+	<script type="text/javascript">
+    $('.toggleform').change(function(e) {
+    	$.ajax(
+			{
+				type: 'post',
+				url: '/includes/managerreport-awaitinginvoice.php',
+				data: $(this).serialize(),
+				success: function(data)
+				{
+				$('#ajax').html(data);
+    			},
+				error: function()
+				{
+				$('#ajax').html('error loading data, please refresh.');
+    			}
+			});
+       e.preventDefault();
+       return false;
+    });
+    </script>
+
