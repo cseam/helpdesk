cfe25d5bf300827dc9f1332c115b2bdac2b51d39
atomicjam <atomicjam@gmail.com>
2015-02-25 12:21:33 +0000

    working towards first beta

--- config/config.php
+++ config/config.php
@@ -12,7 +12,7 @@ define('CODENAME', 'helpdesk');
 define('COMPANY_NAME', 'CLC');
 define('COMPANY_SUFFIX', 'cheltenhamladiescollege.co.uk');
 define('LDAP_SERVER', 'ldap://clcdc1.cheltenhamladiescollege.co.uk');
-define('HELPDESK_LOC', 'http://helpdesk.cheltenhamladiescollege.co.uk');
+define('HELPDESK_LOC', 'http://' . $_SERVER['HTTP_HOST']);
 
 define('DB_LOC', 'localhost');
 define('DB_SCHEMA', 'helpdesk');
