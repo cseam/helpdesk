16954912fbd5acf0e910e1cd02eb82d7de976c5f
atomicjam <atomicjam@gmail.com>
2015-03-26 12:00:54 +0000

    more pdo updates

--- includes/partial/post/update_ticket.php
+++ includes/partial/post/update_ticket.php
@@ -7,70 +7,82 @@
 
 	// Process post
 	if ($_SERVER['REQUEST_METHOD']== "POST") {
-		// echo(print_r($_POST));
-
 		// if Forward ticket
 		if (isset($_POST['forward'])) {
+			//Create update message for db
+			$reason = "<div class=update><h3>Ticket forwarded (".date("l jS \of F Y h:i:s A").") for the following reason,</h3>".$_POST['details']."</div>";
+			//PDO Update ticket
+			$STH = $DBH->Prepare("UPDATE calls SET helpdesk = :helpdesk, assigned = :assigned, lastupdate = :lastupdate, details = CONCAT(details, :details) WHERE callid = :callid");
+			$STH->bindParam(':helpdesk', $_POST['fwdhelpdesk'], PDO::PARAM_STR);
+			$STH->bindParam(':assigned', next_engineer($_POST['fwdhelpdesk']), PDO::PARAM_STR);
+			$STH->bindParam(':lastupdate', date("c"), PDO::PARAM_STR);
+			$STH->bindParam(':details', $reason, PDO::PARAM_STR);
+			$STH->bindParam(':callid', $_POST['id'], PDO::PARAM_STR);
+			$STH->execute();
+
+			//PDO Update engineers assignment
+			$STH = $DBH->Prepare("UPDATE assign_engineers SET engineerId = :engineerid WHERE id = :id");
+			$STH->bindParam(':engineerid', next_engineer($_POST['fwdhelpdesk']), PDO::PARAM_STR);
+			$STH->bindParam(':id', $_POST['fwdhelpdesk'], PDO::PARAM_STR);
+			$STH->execute();
+
 			// Update view
-			echo("<h2>Ticket forwarded</h2>");
-			echo("<p>Ticket #" . $_POST['id'] . " has been forwarded, and details have been updated.</p>");
-			// Update ticket
-			$reasonstr = "<div class=update><h3>Ticket forwarded (".date("l jS \of F Y h:i:s A").") for the following reason,</h3>".$_POST['details']."</div>";
-			// Create SQL for forward (crap need to change to PDO)
-			$sqlstr = "UPDATE calls ";
-			$sqlstr .= "SET ";
-			$sqlstr .= "helpdesk='".$_POST['fwdhelpdesk']."', ";
-			$sqlstr .= "assigned='".next_engineer(check_input($_POST['fwdhelpdesk']))."', ";
-			$sqlstr .= "lastupdate='" . date("c") . "', ";
-			$sqlstr .= "details=CONCAT(details,'".mysqli_real_escape_string($db, $reasonstr)."') ";
-			$sqlstr .= "WHERE callid='" . mysqli_real_escape_string($db, $_POST['id']) . "'";
-			// Run Query
-			mysqli_query($db, $sqlstr);
-			// Update engineers assignment
-			mysqli_query($db, "UPDATE assign_engineers SET engineerId = '". next_engineer(check_input($_POST['fwdhelpdesk'])) ."' WHERE id='".$_POST['fwdhelpdesk']."'");
+			echo("<h2>Ticket Forwarded</h2>".$reason);
 		}
 
 		// if Reassign ticket
 		if (isset($_POST['reassign'])) {
+			//Create update message for db
+			$reason = "<div class=update><h3>Ticket reassigned (".date("l jS \of F Y h:i:s A").") for the following reason,</h3>".$_POST['details']."</div>";
+			//PDO update ticket
+			$STH = $DBH->Prepare("UPDATE calls SET assigned = :assigned, status = 1, lastupdate = :lastupdate, details = CONCAT(details, :details) WHERE callid = :callid");
+			$STH->bindParam(':assigned', $_POST['engineer'], PDO::PARAM_STR);
+			$STH->bindParam(':lastupdate', date("c"), PDO::PARAM_STR);
+			$STH->bindParam(':details', $reason, PDO::PARAM_STR);
+			$STH->bindParam(':callid', $_POST['id'], PDO::PARAM_STR);
+			$STH->execute();
+
 			// Update view
-			echo("<h2>Ticket Reassigned</h2>");
-			echo("<p>Ticket #" . $_POST['id'] . " has been reassigned, and details have been updated.</p>");
-			// Update Message
-			$reasonstr = "<div class=update><h3>Ticket reassigned (".date("l jS \of F Y h:i:s A").") for the following reason,</h3>".$_POST['details']."</div>";
-			// Create SQL for reassign (crap replace with PDO)
-			$sqlstr = "UPDATE calls ";
-			$sqlstr .= "SET assigned=".$_POST['engineer'].", ";
-			$sqlstr .= "status=1, ";
-			$sqlstr .= "lastupdate='" . date("c") . "', ";
-			$sqlstr .= "details=CONCAT(details,'".mysqli_real_escape_string($db, $reasonstr)."') ";
-			$sqlstr .= "WHERE callid='" . mysqli_real_escape_string($db, $_POST['id']) . "'";
-			// Run Query
-			mysqli_query($db, $sqlstr);
+			echo("<h2>Ticket Reassigned</h2>".$reason);
 		}
+
 		// if Close ticket
 		if (isset($_POST['close'])) {
+
 			// Check for image attachments (need to check mime type also)
 			if (is_uploaded_file($_FILES['attachment']['tmp_name']))  {
-			$name_of_uploaded_file = substr(md5(microtime()),rand(0,26),10);
-			$folder = ROOT . UPLOAD_LOC . $name_of_uploaded_file;
-			$tmp_path = $_FILES["attachment"]["tmp_name"];
-			move_uploaded_file($tmp_path, $folder);
-			$upload_img_code = "<img src=" . UPLOAD_LOC . $name_of_uploaded_file . " width=100% />";
+				// rename file to random name to avoid file name clash
+				$name_of_uploaded_file = substr(md5(microtime()),rand(0,26),10);
+				// define uploads folder from config
+				$folder = ROOT . UPLOAD_LOC . $name_of_uploaded_file;
+				// define temp upload location
+				$tmp_path = $_FILES["attachment"]["tmp_name"];
+				// move file from temp location to uploads folder
+				move_uploaded_file($tmp_path, $folder);
+				// create html img tag for ticket detail
+				$upload_img_code = "<img src=" . UPLOAD_LOC . $name_of_uploaded_file . " alt=upload width=100% />";
 			}
-			// Close ticket sql
-			$sqlstr = "UPDATE calls ";
-			$sqlstr .= "SET closed='" . date("c") . "', ";
-			$sqlstr .= "status=2, ";
-			if ($_POST['callreason'] > 0) { $sqlstr .= "callreason=" . $_POST['callreason'] . ", "; };
-			$sqlstr .= "lastupdate='" . date("c") . "', ";
-			$sqlstr .= "closeengineerid='".$_SESSION['engineerId']."',";
-			$sqlstr .= "details='" . mysqli_real_escape_string($db,$_POST['details']) . "<div class=update>"  . $upload_img_code . mysqli_real_escape_string($db,$_POST['updatedetails']) . " <h3> Closed By ".$_SESSION['sAMAccountName'].", " . date("d/m/y h:i") . " </h3></div>'";
-			$sqlstr .= "WHERE callid='" . mysqli_real_escape_string($db,$_POST['id']) . "'";
-			// Run query
-			mysqli_query($db, $sqlstr);
+
+			//Create update message for db
+			$reason = "<div class=update>"  . $upload_img_code . $_POST['updatedetails'] . "<h3>Closed By ".$_SESSION['sAMAccountName'].", " . date("d/m/y h:i") . "</h3></div>'";
+			// PDO update ticket
+			$STH = $DBH->Prepare("UPDATE calls SET closed = :closed, status = 2, callreason = :callreason, lastupdate = :lastupdate, closeengineerid = :closeengineerid, details = CONCAT(details, :details) WHERE callid = :callid");
+			$STH->bindParam(':closed', date("c"), PDO::PARAM_STR);
+			$STH->bindParam(':callreason', $_POST['callreason'], PDO::PARAM_STR);
+			$STH->bindParam(':lastupdate', date("c"), PDO::PARAM_STR);
+			$STH->bindParam(':closeengineerid', $_SESSION['engineerId'], PDO::PARAM_STR);
+			$STH->bindParam(':details', $reason, PDO::PARAM_STR);
+			$STH->bindParam(':callid', $_POST['id'], PDO::PARAM_STR);
+			$STH->execute();
+
 			// Email stakeholder
-			$contact = mysqli_query($db, "SELECT email FROM calls WHERE callid='".$_POST['id']."'");
-			$row = $contact->fetch_object();
+			// Get email from ticket details
+			$STH = $DBH->Prepare('SELECT email FROM calls WHERE callid = :callid');
+			$STH->bindParam(':callid', $_POST['id'], PDO::PARAM_INT);
+			$STH->setFetchMode(PDO::FETCH_OBJ);
+			$STH->execute();
+			$row = $STH->fetch();
+			// Construct message
 			$to = $row->email;
 			$message = "<p>Helpdesk (#" . $_POST['id'] .") has been closed</p>";
 			$message .= "<p>To view the details of this ticket please <a href='". HELPDESK_LOC ."'>Visit ". CODENAME ."</a></p>";
@@ -82,11 +94,12 @@
 			$headers .= 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 			$headers .= 'X-Mailer: PHP/' . phpversion();
-			// In case any of our lines are larger than 70 characters, we should use wordwrap()
+			// In case any of our lines are larger than 70 characters, we wordwrap()
 			$message = wordwrap($message, 70, "\r\n");
 			// Send email
 			mail($to, $msgtitle, $message, $headers);
-			// update view
+
+			// Update view
 			echo("<h2>Updated & Closed</h2>");
 			echo("<p>Ticket #" . $_POST['id'] . " has been updated and closed.</p>");
 				SWITCH ($_SESSION['engineerLevel']) {
@@ -106,27 +119,34 @@
 		if (isset($_POST['update'])) {
 			// Check for image attachments (need to check mime type also)
 			if (is_uploaded_file($_FILES['attachment']['tmp_name']))  {
-			$name_of_uploaded_file = substr(md5(microtime()),rand(0,26),10);
-			$folder = ROOT . UPLOAD_LOC . $name_of_uploaded_file;
-			$tmp_path = $_FILES["attachment"]["tmp_name"];
-			move_uploaded_file($tmp_path, $folder);
-			$upload_img_code = "<img src=" . UPLOAD_LOC . $name_of_uploaded_file . " width=100% />";
+				// rename file to random name to avoid file name clash
+				$name_of_uploaded_file = substr(md5(microtime()),rand(0,26),10);
+				// define uploads folder from config
+				$folder = ROOT . UPLOAD_LOC . $name_of_uploaded_file;
+				// define temp upload location
+				$tmp_path = $_FILES["attachment"]["tmp_name"];
+				// move file from temp location to uploads folder
+				move_uploaded_file($tmp_path, $folder);
+				// create html img tag for ticket detail
+				$upload_img_code = "<img src=" . UPLOAD_LOC . $name_of_uploaded_file . " alt=upload width=100% />";
 			}
 
-			// update ticket
-			$sqlupdatestr = "UPDATE calls ";
-			$sqlupdatestr .= "SET status=1, ";
-			$sqlupdatestr .= "lastupdate='" . date("c") . "', ";
-			$sqlupdatestr .= "closed=NULL, ";
-			$sqlupdatestr .= "callreason=NULL, ";
-			$sqlupdatestr .= "details='".  mysqli_real_escape_string($db,$_POST['details']) . "<div class=update>" . $upload_img_code .  mysqli_real_escape_string($db,$_POST['updatedetails']) . " <h3> Update By ".$_SESSION['sAMAccountName'].", " . date("d/m/y h:i") . "</h3></div>'";
-			$sqlupdatestr .= "WHERE callid='" . mysqli_real_escape_string($db,$_POST['id']) . "'";
-			// Run query
-			mysqli_query($db, $sqlupdatestr);
+			// Create update message for db
+			$reason = "<div class=update>" . $upload_img_code . $_POST['updatedetails'] . "<h3> Update By ".$_SESSION['sAMAccountName'].", " . date("d/m/y h:i") . "</h3></div>";
+			// PDO update ticket
+			$STH = $DBH->Prepare("UPDATE calls SET status = 1, lastupdate = :lastupdate, closed = NULL, callreason = NULL, details = CONCAT(details, :details) WHERE callid = :callid");
+			$STH->bindParam(':lastupdate', date("c"), PDO::PARAM_STR);
+			$STH->bindParam(':details', $reason, PDO::PARAM_STR);
+			$STH->bindParam(':callid', $_POST['id'], PDO::PARAM_STR);
+			$STH->execute();
 
-			// Email user
-			$contact = mysqli_query($db, "SELECT email FROM calls WHERE callid='".$_POST['id']."'");
-			$row = $contact->fetch_object();
+			// Get email from ticket details
+			$STH = $DBH->Prepare('SELECT email FROM calls WHERE callid = :callid');
+			$STH->bindParam(':callid', $_POST['id'], PDO::PARAM_INT);
+			$STH->setFetchMode(PDO::FETCH_OBJ);
+			$STH->execute();
+			$row = $STH->fetch();
+			// Construct message
 			$to = $row->email;
 			$message = "<p>Your helpdesk (#" . $_POST['id'] .") has been updated</p>";
 			$message .= "<p>To view the details of this update or update your ticket please <a href='". HELPDESK_LOC ."'>Visit ". CODENAME ."</a></p>";
@@ -137,12 +157,11 @@
 			$headers .= 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 			$headers .= 'X-Mailer: PHP/' . phpversion();
-			// In case any of our lines are larger than 70 characters, we should use wordwrap()
+			// In case any of our lines are larger than 70 characters, we wordwrap()
 			$message = wordwrap($message, 70, "\r\n");
 			// Send email
 			mail($to, $msgtitle, $message, $headers);
 			// update view
-			echo("<h2>Ticket updated</h2>");
-			echo("<p>Ticket #" . $_POST['id'] . " has been updated, email sent to update users.</p>");
+			echo("<h2>Ticket updated</h2><p>Ticket #" . $_POST['id'] . " has been updated, email sent to update users.</p>");
 		}
 	}
