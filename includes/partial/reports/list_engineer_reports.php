25d1044152269900f370a58ee31607f4072c3cd1
atomicjam <atomicjam@gmail.com>
2015-09-24 11:23:39 +0100

    added workrate for engineers

--- includes/partial/reports/list_engineer_reports.php
+++ includes/partial/reports/list_engineer_reports.php
@@ -5,4 +5,5 @@
 	<a href="#" onclick="update_div('#ajax','reports/view_lockers.php');"><img src="/public/images/ICONS-lockers@2x.png" alt="Lockers" title="Lockers"  width="24" height="25" />Lockers</a>
 	<a href="#" onclick="update_div('#ajax','reports/view_change_control.php');"><img src="/public/images/ICONS-changecontrol@2x.png" alt="View change control" title="View change control"  width="24" height="25" />Change Control</a>
 	<a href="#" onclick="update_div('#ajax','reports/list_all_outofhours.php');"><img src="/public/images/ICONS-outofhours@2x.png" alt="Out Of Hours" title="Out Of Hours"  width="24" height="25" />Out Of Hours</a>
+	<a href="#" onclick="update_div('#ajax','reports/view_work_rate.php');"><img src="/public/images/ICONS-workrate@2x.png" alt="Engineer work rate" title="Engineer work rate"  width="24" height="25" />Work rate</a>
 </div>
