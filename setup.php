a50f2a8ffb0ca8840adda9aabc061ea68bec9e4c
atomicjam <atomicjam@gmail.com>
2016-01-18 14:27:05 +0000

    updated setup script

--- setup.php
+++ setup.php
@@ -1,37 +1,397 @@
 <?php
 // HELPDESK SETUP SCRIPT
-// used to create initial database, delete this setup.php once you have run successfully
+// Used to create initial databases, delete this setup.php once you have run successfully
 
+// Load config
+// Ensure config/config.php is updated with correct mysql details before this script is run
+include_once($_SERVER['DOCUMENT_ROOT'] . '/config/config.php');
 
-//$servername = "localhost";
-//$username = "username";
-//$password = "password";
-//$dbname = "myDBPDO";
+// try to create tables
+try {
+	// Connect to dev db
+	$conn = new PDO("mysql:host=".DB_LOC.";dbname=".DEV_DB_SCHEMA, DB_USER, DB_PASSWORD);
+	// set the PDO error mode to exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// sql to create tables
+	
+	// create assign engineers
+	$sql = "
+		CREATE TABLE `assign_engineers` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		`engineerId` int(11) NOT NULL,
+		PRIMARY KEY (`id`),
+		UNIQUE KEY `idengineers_UNIQUE` (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+	";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(assign_engineers) created successfully</p>";
+	
+	// create call_additional_fields
+	$sql = "
+		CREATE TABLE `call_additional_fields` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`typeid` int(11) DEFAULT NULL,
+		`label` varchar(45) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+	";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(call_additional_fields) created successfully</p>";
+	
+	// create call additional results
+	$sql = "
+		CREATE TABLE `call_additional_results` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`callid` int(11) DEFAULT NULL,
+		`label` varchar(45) DEFAULT NULL,
+		`value` varchar(45) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+	";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(call_additional_results) created successfully</p>";
 
-//try {
-//    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-/*
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// create call_views
+	$sql = "
+		CREATE TABLE `call_views` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`sAMAccountName` varchar(45) DEFAULT NULL,
+		`callid` int(11) DEFAULT NULL,
+		`stamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+	";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(call_views) created successfully</p>";
 
-    // sql to create table
-    $sql = "CREATE TABLE MyGuests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP
-    )";
+	// create call reasons
+	$sql = "
+		CREATE TABLE `callreasons` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`reason_name` varchar(255) DEFAULT '',
+		`helpdesk_id` int(11) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(call reasons) created successfully</p>";
 
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "Table MyGuests created successfully";
-    }
+	// create calls
+	$sql = "
+		CREATE TABLE `calls` (
+		`callid` int(11) NOT NULL AUTO_INCREMENT,
+		`name` varchar(45) DEFAULT NULL,
+		`email` varchar(255) DEFAULT NULL,
+		`tel` varchar(45) DEFAULT NULL,
+		`details` longtext,
+		`assigned` int(11) DEFAULT NULL,
+		`opened` datetime DEFAULT NULL,
+		`lastupdate` datetime DEFAULT NULL,
+		`closed` datetime DEFAULT NULL,
+		`status` int(11) DEFAULT '1',
+		`urgency` int(11) DEFAULT '2',
+		`location` varchar(45) DEFAULT NULL,
+		`room` varchar(45) DEFAULT NULL,
+		`category` int(11) DEFAULT NULL,
+		`closeengineerid` int(11) DEFAULT NULL,
+		`owner` varchar(45) DEFAULT NULL,
+		`helpdesk` int(11) DEFAULT NULL,
+		`invoicedate` datetime DEFAULT NULL,
+		`callreason` int(11) DEFAULT NULL,
+		`title` longtext,
+		`lockerid` int(11) DEFAULT NULL,
+		`pm` int(11) DEFAULT '0',
+		`requireinvoice` int(11) DEFAULT '0',
+		PRIMARY KEY (`callid`),
+		UNIQUE KEY `callid_UNIQUE` (`callid`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(calls) created successfully</p>";
+
+	// create categories
+	$sql = "
+		CREATE TABLE `categories` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`categoryName` varchar(45) DEFAULT NULL,
+		`helpdesk` int(11) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(categories) created successfully</p>";
+
+	// create change control 
+	$sql = "
+		CREATE TABLE `changecontrol` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`engineersid` int(11) DEFAULT NULL,
+		`stamp` datetime DEFAULT NULL,
+		`changemade` longtext,
+		`tags` varchar(45) DEFAULT '',
+		`server` varchar(45) DEFAULT NULL,
+		`helpdesk` int(11) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(change control) created successfully</p>";
+
+	// create change control tags 
+	$sql = "
+		CREATE TABLE `changecontrol_tags` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`tagname` varchar(45) DEFAULT NULL,
+		`helpdesk` int(11) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(change control tags) created successfully</p>";
+
+	// create engineers 
+	$sql = "
+		CREATE TABLE `engineers` (
+		`idengineers` int(11) NOT NULL AUTO_INCREMENT,
+		`engineerName` varchar(45) NOT NULL,
+		`engineerEmail` varchar(45) NOT NULL,
+		`availableDays` varchar(45) NOT NULL DEFAULT '1,2,3,4,5,6,7',
+		`sAMAccountName` varchar(45) NOT NULL DEFAULT '',
+		`engineerLevel` int(11) NOT NULL DEFAULT '1',
+		`helpdesk` int(11) NOT NULL,
+		`superuser` int(11) DEFAULT NULL,
+		`disabled` int(11) DEFAULT '0',
+		PRIMARY KEY (`idengineers`),
+		UNIQUE KEY `idengineers_UNIQUE` (`idengineers`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(engineers) created successfully</p>";
+
+	// create engineers punchcard 
+	$sql = "
+		CREATE TABLE `engineers_punchcard` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`direction` int(11) NOT NULL,
+		`stamp` datetime NOT NULL,
+		`engineerid` int(11) DEFAULT NULL,
+		`note` varchar(45) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(engineers punchcard) created successfully</p>";
+
+	// create engineer status
+	$sql = "
+		CREATE TABLE `engineers_status` (
+		`id` int(11) unsigned NOT NULL DEFAULT '0',
+		`status` int(11) NOT NULL DEFAULT '0',
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(engineers status) created successfully</p>";
+		
+	// create feedback 
+	$sql = "
+		CREATE TABLE `feedback` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`callid` int(11) DEFAULT NULL,
+		`satisfaction` varchar(45) DEFAULT NULL,
+		`details` longtext,
+		`opened` datetime DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(feedback) created successfully</p>";
+		
+	// create helpdesks
+	$sql = "
+		CREATE TABLE `helpdesks` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`helpdesk_name` varchar(45) DEFAULT NULL,
+		`description` varchar(2000) DEFAULT NULL,
+		`deactivate` int(11) NOT NULL DEFAULT '0',
+		`auto_assign` int(11) DEFAULT '0',
+		`email_on_newticket` int(11) DEFAULT '0',
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(helpdesks) created successfully</p>";
+
+	// create location 
+	$sql = "
+		CREATE TABLE `location` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`locationName` varchar(45) DEFAULT NULL,
+		`iconlocation` varchar(255) DEFAULT NULL,
+		`shorthand` varchar(45) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(location) created successfully</p>";
+
+	// create out of hours 
+	$sql = "
+		CREATE TABLE `out_of_hours` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`name` varchar(255) DEFAULT NULL,
+		`dateofcall` varchar(255) DEFAULT NULL,
+		`timeofcall` varchar(255) DEFAULT NULL,
+		`calloutby` varchar(255) DEFAULT NULL,
+		`problem` text,
+		`previsit` text,
+		`timeonsite` varchar(255) DEFAULT NULL,
+		`timeleftsite` varchar(255) DEFAULT NULL,
+		`locations` varchar(255) DEFAULT NULL,
+		`resolution` text,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(Out of hours) created successfully</p>";
+
+	// create out of hours contact 
+	$sql = "
+		CREATE TABLE `out_of_hours_contact_details` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`helpdesk` int(11) DEFAULT NULL,
+		`end_of_day` int(11) DEFAULT NULL,
+		`message` text,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(Out of hours contact) created successfully</p>";
+
+	// create performance objectives 
+	$sql = "
+		CREATE TABLE `performance_review_objectives` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`engineerid` int(11) DEFAULT NULL,
+		`title` longtext,
+		`details` longtext,
+		`progress` varchar(255) DEFAULT '0',
+		`datedue` datetime DEFAULT NULL,
+		`status` int(11) DEFAULT '1',
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(Performance objectives) created successfully</p>";
+
+	// create quick responses 
+	$sql = "
+		CREATE TABLE `quick_responses` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`quick_response` varchar(255) DEFAULT NULL,
+		`helpdesk_id` int(11) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(quick responses) created successfully</p>";
+
+	// create scheduled calls 
+	$sql = "
+		CREATE TABLE `scheduled_calls` (
+		`callid` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`name` varchar(45) DEFAULT NULL,
+		`email` varchar(45) DEFAULT NULL,
+		`tel` varchar(45) DEFAULT NULL,
+		`details` longtext,
+		`assigned` int(11) DEFAULT NULL,
+		`opened` datetime DEFAULT NULL,
+		`lastupdate` datetime DEFAULT NULL,
+		`closed` datetime DEFAULT NULL,
+		`status` int(11) DEFAULT NULL,
+		`urgency` int(11) DEFAULT NULL,
+		`location` varchar(45) DEFAULT NULL,
+		`room` varchar(45) DEFAULT NULL,
+		`category` int(11) DEFAULT NULL,
+		`closeengineerid` int(11) DEFAULT NULL,
+		`owner` varchar(45) DEFAULT NULL,
+		`helpdesk` int(11) DEFAULT NULL,
+		`invoicedate` datetime DEFAULT NULL,
+		`callreason` int(11) DEFAULT NULL,
+		`title` longtext,
+		`lockerid` int(11) DEFAULT NULL,
+		`frequencytype` varchar(45) DEFAULT NULL,
+		`startschedule` datetime DEFAULT NULL,
+		PRIMARY KEY (`callid`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(scheduled calls) created successfully</p>";
+
+	// create scheduled calls cron 
+	$sql = "
+		CREATE TABLE `scheduled_calls_cron_log` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`message` varchar(255) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(scheduled calls cron) created successfully</p>";
+
+	// create service level agreement 
+	$sql = "
+		CREATE TABLE `service_level_agreement` (
+		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+		`helpdesk` int(11) DEFAULT NULL,
+		`urgency` int(11) DEFAULT NULL,
+		`agreement` varchar(255) DEFAULT NULL,
+		`close_eta_days` int(11) DEFAULT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(service level agreement) created successfully</p>";
+
+	// create status 
+	$sql = "
+		CREATE TABLE `status` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		`statusCode` varchar(45) NOT NULL DEFAULT '',
+		PRIMARY KEY (`id`),
+		UNIQUE KEY `idengineers_UNIQUE` (`id`)
+		) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+		";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+		echo "<p>(status) created successfully</p>";
+	
+	}
 catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
-
+	{
+	echo $sql . "<br>" . $e->getMessage();
+	}
 $conn = null;
-*/
 ?>
