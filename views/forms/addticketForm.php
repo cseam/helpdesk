77f1925ee84bc2e333b1f8e0e9e2618b8772dfb1
atomicjam <atomicjam@gmail.com>
2016-03-22 17:03:45 +0000

    wip

--- views/forms/addticketForm.php
+++ views/forms/addticketForm.php
@@ -5,7 +5,6 @@
 		<legend>Contact details</legend>
 			<label for="name" title="Contact name for this call">Your/Contact Name</label>
 			<input type="text" id="name" name="name" value=""  required />
-			<p class="note">Please enter your name, not a generic house name; then engineer can contact you directly if any problems arise.</p>
 			<label for="contact_email" title="Contact email so engineer can comunicate">Contact Email</label>
 			<input type="text" id="contact_email" name="contact_email" value="<?php echo $_SESSION['sAMAccountName']."@". COMPANY_SUFFIX;?>"  required />
 			<label for="tel" title="Contact telephone so engineer can comunicate">Telephone / Mobile Number</label>
