1dd5fcec382ae535e72de11af5d0bd82a8d80beb
atomicjam <atomicjam@gmail.com>
2016-04-18 11:17:25 +0100

    fixes #44

--- controllers/actions/actionEngineerDefault.php
+++ controllers/actions/actionEngineerDefault.php
@@ -7,9 +7,17 @@ class actionEngineerDefault {
     $left = new leftpageController();
 
     $ticketModel = new ticketModel();
+    $callreasonsModel = new callreasonsModel();
+    $quickresponseModel = new quickresponseModel();
     //populate page content with oldest open ticket
     $ticketDetails = $ticketModel->getOldestTicketByEngineer($_SESSION['engineerId']);
-
+    $additionalDetails = $ticketModel->getAdditionalDetails($ticketDetails["callid"]);
+    //populate call reasons for this tickets helpdeskid
+    $callreasons = $callreasonsModel->getReasonsByHelpdeskId($ticketDetails["helpdesk"]);
+    //populate quick responses
+    $quickresponse = $quickresponseModel->getQuickResponseByHelpdeskId($ticketDetails["helpdesk"]);
+    //update ticket views with user id and time stamp
+    $ticketModel->logViewTicketById($ticketDetails["callid"], $_SESSION['sAMAccountName']);
     //render page
     require_once "views/engineerView.php";
   }
