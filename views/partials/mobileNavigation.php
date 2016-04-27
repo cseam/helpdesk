6d39574841819bb11646ab7d0a4e7a5b90a9a282
atomicjam <atomicjam@gmail.com>
2016-04-27 11:01:52 +0100

    bugfixes

--- views/partials/mobileNavigation.php
+++ views/partials/mobileNavigation.php
@@ -1,21 +1,14 @@
 <div id="sidr">
 <h3>Controls</h3>
 <a href="/ticket/add/">Add Ticket</a> / <a href="/user/">My Tickets</a><br/>
+<?php if (isset($_SESSION['engineerLevel'])) { ?>
 <?php if ($_SESSION['engineerLevel'] === "1") { ?><a href="/engineer/">Engineer View</a><br/><?php }; ?>
 <?php if ($_SESSION['engineerLevel'] === "2" or $_SESSION['superuser'] === "1") { ?><a href="/manager/">Manager View</a><br/><?php }; ?>
 <?php if ($_SESSION['engineerLevel'] === "2" or $_SESSION['superuser'] === "1") { ?><a href="/report/">Reports View</a><br/><?php }; ?>
 <?php if ($_SESSION['superuser'] === "1") { ?><a href="/admin/">Admin View</a><br/><?php }; ?>
 <a href="/logout/" class="logout">Log out</a><br/>
 <br/>
-<?php if ($_SESSION['engineerLevel'] === "0") {
-	//this section isnt required can be removed once tested
-	?>
-<h3>My Tickets</h3>
-	<a href="#calllist" onclick="$('#leftpage').animate({scrollTop: $('#calllist').offset().top-130}, 2000);" class="menubutton"><img src="/public/images/ICONS-yourcalls.svg" alt="your tickets" title="your tickets"  width="16" height="17" /> Assigned Tickets</a><br/>
-	<a href="#deptlist" onclick="$('#leftpage').animate({scrollTop: $('#deptlist').offset().top-130}, 2000);" class="menubutton"><img src="/public/images/ICONS-allcalls.svg" alt="your tickets" title="your tickets"  width="16" height="17" /> Departments Tickets</a><br/>
-	<a href="#objlist" onclick="$('#leftpage').animate({scrollTop: $('#objlist').offset().top-130}, 2000);" class="menubutton"><img src="/public/images/ICONS-allcalls.svg" alt="Performance Objectives" title="Performance Objectives"  width="16" height="17" />Performance Objectives</a><br/>
-	<a href="#reportlinks" onclick="$('#leftpage').animate({scrollTop: $('#reportlinks').offset().top-130}, 2000);" class="menubutton"><img src="/public/images/ICONS-workrate.svg" alt="your tickets" title="your tickets"  width="16" height="17" /> Reports</a><br/>
-<?php }; ?>
+<?php } ?>
 </div>
 <script type="text/javascript">
 	$(function() {
