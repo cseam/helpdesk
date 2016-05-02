0dafdb1dcebad06bdad3812ab1db160e497518ef
atomicjam <atomicjam@gmail.com>
2016-05-02 15:02:18 +0100

    wip #77

--- models/locationModel.php
+++ models/locationModel.php
@@ -14,15 +14,19 @@
     }
 
     public function removeLocationById($id) {
-
+      $database = new Database();
+      $database->query("DELETE FROM location WHERE id=:id");
+      $database->bind(':id', $id);
+      $database->execute();
+      return true;
     }
 
     public function addLocation($locationobject) {
-
+      $database = new Database();
     }
 
     public function modifyLocationById($id, $locationobject) {
-      
+      $database = new Database();
     }
 
 }
