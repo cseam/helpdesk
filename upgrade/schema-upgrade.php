45d4af0a016cefc0fcf4f7774b811aa5a0191006
atomicjam <atomicjam@gmail.com>
2016-05-23 15:40:34 +0100

    wip

--- upgrade/schema-upgrade.php
+++ upgrade/schema-upgrade.php
@@ -12,20 +12,22 @@
     Used to upgrade ticket details from single record to split ones
   </p>
   <p>
-    listed are all updates for all tickets split out 
+    listed are all updates for all tickets split out
   </p>
-
 <style>
   table
   {
       table-layout: fixed;
       width: 100%;
   }
+  ::-webkit-scrollbar {
+  	width: 25px;
+  }
 </style>
-
   <table>
     <thead>
       <tr>
+        <th>DB Row</th>
         <th>Call ID</th>
         <th>Update</th>
         <th>Calculated Timestamp</th>
@@ -33,10 +35,12 @@
     </thead>
     <tbody>
 <?php
+  // up memory size to process request
+  ini_set('memory_limit', '1024M');
   //hard code fudge as not using framework for this but this page will be destroyed once upgrade is done
-  require_once "config/config.php";
-  require_once "models/ticketModel.php";
-  require_once "models/Database.php";
+  require_once "../config/config.php";
+  require_once "../models/ticketModel.php";
+  require_once "../models/Database.php";
   $ticketModel = new ticketModel();
 
   function getNodeInnerHTML(DOMNode $oNode)
@@ -46,10 +50,11 @@
       return $oDom->saveHTML();
   }
 
-
+  $counter = 0;
   // get all tickets to process
-  //$ticketDetails = $ticketModel->getAllTicketsNoLimit();
-  $ticketDetails = $ticketModel->getAllTickets(1000);
+  $ticketDetails = $ticketModel->getAllTicketsNoLimit();
+  // get 10 tickets used when testing instead of getting 20,000 records
+  // $ticketDetails = $ticketModel->getAllTickets(100);
   // loop all tickets
   foreach ($ticketDetails as $key => $value) {
 
@@ -59,11 +64,28 @@
         $items = $doc->documentElement->getElementsByTagName('div');
             foreach ($items as $tag1)
             {
+              $counter++;
               echo "<tr>";
+              echo "<td>".$counter."</td>";
               echo "<td>" . $value["callid"] . "</td>";
               echo "<td>" . getNodeInnerHTML($tag1) . "</td>";
+
+              echo "<td>";
               // need to parse date and time from $tag1 else output $value[opened]
-              echo "<td>" . $value["opened"] . "</td>";
+              if (DateTime::createFromFormat('d/m/Y H:i', substr($tag1->nodeValue, -16)) !== FALSE) {
+                // it's a date
+                $calcDT = DateTime::createFromFormat('d/m/Y H:i', substr($tag1->nodeValue, -16));
+                //echo substr($tag1->nodeValue, -16);
+                echo "<span style=\"color: orange\">" . $calcDT->format("Y-m-d H:i:s") . "</span>";
+              } else {
+                echo $value["opened"];
+              }
+
+
+               echo "</td>";
+
+
+
               echo "</tr>";
             }
 
@@ -73,8 +95,6 @@
 
   </tbody>
 </table>
-
-
 </div>
 </body>
 </html>
