cf0d19bc546cf6f0bfde6c6bcd11e8810cf5c461
atomicjam <atomicjam@gmail.com>
2016-07-05 15:01:51 +0100

    wip #154

--- controllers/reportOpenController.php
+++ controllers/reportOpenController.php
@@ -3,23 +3,25 @@
 class reportOpenController {
   public function __construct()
   {
-    //load content for left side of page
-    $left = new leftpageController();
     //create new models for required data
     $ticketModel = new ticketModel();
     $helpdeskModel = new helpdeskModel();
-    $pagedata = new stdClass();
-    //set report name
-    $reportname = "Open";
+    //create empty object to store data for template
+    $templateData = new stdClass();
+
     //set report title
-    $pagedata->title = $reportname . " Tickets";
-    //populate report results for use in view
-    $pagedata->reportResults = $ticketModel->getOpenTicketsByHelpdesk($_SESSION['engineerHelpdesk']);
+    $templateData->title = "Open tickets";
     //get helpdesk details
     $helpdeskdetails = $helpdeskModel->getFriendlyHelpdeskName($_SESSION['engineerHelpdesk']);
-    //set page details
-    $pagedata->details = sizeof($pagedata->reportResults)." ".$reportname." tickets for ".$helpdeskdetails["helpdesk_name"]." helpdesk.";
-    //render template using $pagedata object
-    require_once "views/reports/resultsListReportView.php";
+    //populate report results for use in view
+    $templateData->reportResults = $ticketModel->getOpenTicketsByHelpdesk($_SESSION['engineerHelpdesk']);
+    //set summary details
+    $templateData->details = sizeof($templateData->reportResults). " " . $templateData->title . " for ".$helpdeskdetails["helpdesk_name"]." helpdesk.";
+
+    //pass complete data and template to view engine and render
+    $view = new Page();
+    $view->setTemplate('reports/resultsListReportView');
+    $view->setDataSrc($templateData);
+    $view->render();
   }
 }
