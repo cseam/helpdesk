26e79bd0027003b3c74eb658b214d4240f0a5561
atomicjam <atomicjam@gmail.com>
2016-07-08 11:43:59 +0100

    wip #136

--- controllers/reportDefaultController.php
+++ controllers/reportDefaultController.php
@@ -9,20 +9,20 @@ class reportDefaultController {
     $templateData = new stdClass();
 
     //set report title
-    $templateData->title = "Reports overview";
+    $templateData->title = "Reports Summary";
     //set page details
     $templateData->details = $templateData->title. " showing helpdesk activity, ";
     if (isset($_SESSION['customReportsRangeStart'])) { $templateData->details .= " from " . $_SESSION['customReportsRangeStart'] . " to " . $_SESSION['customReportsRangeEnd']; } else { $templateData->details .= " this month."; }
     //populate report results for use in view
     //TODO pull data from models hard coded while testing
-    $templateData->opentickets = 10;
-    $templateData->closedtickets = 200;
-    $templateData->activity = 7;
+    $templateData->opentickets = $ticketModel->countOutstandingTicketsByHelpdesk($_SESSION['engineerHelpdesk']);
+    $templateData->closedtickets = $ticketModel->countClosed($_SESSION['engineerHelpdesk']);
+    $templateData->activity = $ticketModel->countActivity($_SESSION['engineerHelpdesk']);
     $templateData->firstresponse = 90;
     $templateData->closetime = 100;
-    $templateData->avgfeedback = 4.3;
-    $templateData->topcategory = "Software";
-    $templateData->avgurgency = "Low";
+    $templateData->avgfeedback = $ticketModel->avgFeedback($_SESSION['engineerHelpdesk']);
+    $templateData->topcategory = $ticketModel->topCategory($_SESSION['engineerHelpdesk']);
+    $templateData->avgurgency = $ticketModel->avgUrgency($_SESSION['engineerHelpdesk']);
     //pass complete data and template to view engine and render
     $view = new Page();
     $view->setTemplate('resultsOverview');
--- views/resultsOverview.php
+++ views/resultsOverview.php
@@ -13,13 +13,13 @@
         <div id="overview">
         <a href="/report/outstanding/">
           <p>
-            <span class="overviewValue"><?php echo @$pagedata->opentickets ?></span>
+            <span class="overviewValue"><?php echo number_format(@$pagedata->opentickets["outstanding"],0) ?></span>
             <span class="overviewLabel">Open tickets</span>
           </p>
         </a>
         <a href="/report/engineerbreakdown/">
           <p>
-            <span class="overviewValue"><?php echo @$pagedata->closedtickets ?></span>
+            <span class="overviewValue"><?php echo number_format(@$pagedata->closedtickets["countClosed"],0) ?></span>
             <span class="overviewLabel">Closed tickets</span>
           </p>
         </a>
@@ -29,27 +29,14 @@
               <script type="text/javascript">
                 $(function() {
                   var activityData = {
-                    labels: ['7am','8','9','10','11','12','13','14','15','16','17','18','19pm'],
-                    series: [ [<?php echo @$pagedata->activity-5 ?>,
-                              <?php echo @$pagedata->activity-2 ?>,
-                              <?php echo @$pagedata->activity ?>,
-                              <?php echo @$pagedata->activity-2 ?>,
-                              <?php echo @$pagedata->activity-5 ?>,
-                              <?php echo @$pagedata->activity-5 ?>,
-                              <?php echo @$pagedata->activity-2 ?>,
-                              <?php echo @$pagedata->activity-2 ?>,
-                              <?php echo @$pagedata->activity ?>,
-                              <?php echo @$pagedata->activity-4 ?>,
-                              <?php echo @$pagedata->activity-4 ?>,
-                              <?php echo @$pagedata->activity-4 ?>,
-                              <?php echo @$pagedata->activity-5 ?>]
-                            ]
+                    labels: ['<7am','7','8','9','10','11','12','13','14','15','16','17','18','>19pm'],
+                    series: [ [<?php echo $pagedata->activity["0-7"] ?>,<?php echo $pagedata->activity["7-8"] ?>,<?php echo $pagedata->activity["8-9"] ?>,<?php echo $pagedata->activity["9-10"] ?>,<?php echo $pagedata->activity["10-11"] ?>,<?php echo $pagedata->activity["11-12"] ?>,<?php echo $pagedata->activity["12-13"] ?>,<?php echo $pagedata->activity["13-14"] ?>,<?php echo $pagedata->activity["14-15"] ?>,<?php echo $pagedata->activity["15-16"] ?>,<?php echo $pagedata->activity["16-17"] ?>,<?php echo $pagedata->activity["17-18"] ?>,<?php echo $pagedata->activity["18-19"] ?>,<?php echo $pagedata->activity["19-24"] ?>] ]
                   }
                   var activityOptions = {
                     showPoint: true,
                     lineSmooth: true,
                     fullWidth: true,
-                    chartPadding: { right: 30, left: 30, bottom: 0 },
+                    chartPadding: { right: 35, left: 35, bottom: 0 },
                     axisX: { showGrid: false },
                     axisY: { showLabel: false, showGrid: false },
                     low: 0
@@ -75,20 +62,23 @@
           </p>
         </a>
         <a href="/report/feedback/">
-          <p class="fullwidth">
-            <span class="overviewValue"><?php for ($i = 0; $i < round(@$pagedata->avgfeedback); $i++) { echo "<img src='/public/images/ICONS-star.svg' alt='star' height='75' width='auto' />"; } ?></span>
+          <p class="fullwidth" style="padding: 35px 0;">
+            <span class="overviewValue">
+              <?php echo (@$pagedata->avgfeedback["avgFeedback"]) ? null : "no feedback left" ?>
+              <?php for ($i = 0; $i < round(number_format(@$pagedata->avgfeedback["avgFeedback"],2)); $i++) { echo "<img src='/public/images/ICONS-star.svg' alt='star' height='75' width='auto' />"; } ?>
+            </span>
             <span class="overviewLabel">Average feedback</span>
           </p>
         </a>
         <a href="/report/categorybreakdown/">
           <p>
-            <span class="overviewValue"><?php echo @$pagedata->topcategory ?></span>
+            <span class="overviewValue"><?php echo @$pagedata->topcategory["categoryName"] ?></span>
             <span class="overviewLabel">Top category</span>
           </p>
         </a>
         <a href="/report/urgencybreakdown/">
           <p>
-            <span class="overviewValue"><?php echo @$pagedata->avgurgency ?></span>
+            <span class="overviewValue"><?php echo urgency_friendlyname(number_format(@$pagedata->avgurgency["avgUrgency"],0)) ?></span>
             <span class="overviewLabel">Average urgency</span>
           </p>
         </a>
