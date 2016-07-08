315171339b17c5ed79ac10d231a638135e3c93d9
atomicjam <atomicjam@gmail.com>
2016-07-08 15:57:31 +0100

    tidy up

--- controllers/reportDefaultController.php
+++ controllers/reportDefaultController.php
@@ -17,11 +17,9 @@ class reportDefaultController {
     $templateData->details = $templateData->title. " showing helpdesk activity, ";
     if (isset($_SESSION['customReportsRangeStart'])) { $templateData->details .= " from " . $_SESSION['customReportsRangeStart'] . " to " . $_SESSION['customReportsRangeEnd']; } else { $templateData->details .= " this month."; }
     //populate report results for use in view
-    //TODO pull data from models hard coded while testing
     $templateData->opentickets = $ticketModel->countOutstandingTicketsByHelpdesk($_SESSION['engineerHelpdesk']);
     $templateData->closedtickets = $ticketModel->countClosed($_SESSION['engineerHelpdesk']);
     $templateData->activity = $ticketModel->countActivity($_SESSION['engineerHelpdesk']);
-
     $SLAtotals = new stdClass();
     for ($i=1; $i<=10; $i++) {
       // hard coded 10 SLAs {clown fiesta should not be hard coded FIX THIS}
@@ -33,7 +31,6 @@ class reportDefaultController {
       $first += $value["FirstResponseSuccess"];
       $close += $value["ResponseTimeSuccess"];
     }
-
     $templateData->firstresponse = number_format(($total !== 0 ? ($first / $total) : 0) * 100 ,2);
     $templateData->closetime = number_format(($total !== 0 ? ($close / $total) : 0) * 100 ,2);
     $templateData->avgfeedback = $feedbackModel->avgFeedback($_SESSION['engineerHelpdesk']);
