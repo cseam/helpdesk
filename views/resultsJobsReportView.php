67a9c5df7fd1dd74faa05fb43986468b0a3e0318
atomicjam <atomicjam@gmail.com>
2016-08-25 14:51:58 +0100

    Bug fix

--- views/resultsJobsReportView.php
+++ views/resultsJobsReportView.php
@@ -10,36 +10,36 @@
       <div id="ajax">
         <h1><?php echo $pagedata->title ?></h1>
         <p><?php echo $pagedata->details ?></p>
-
         <table id="yourcalls">
             <?php
             // count for engineers totals
             $tmp_array = array();
             foreach($pagedata->reportResults as &$value) { array_push($tmp_array, $value["engineerName"]); }
             @$outputCount = array_count_values($tmp_array);
-
             $engineergroup = 0;
             if (isset($pagedata->reportResults)) {
+
             foreach($pagedata->reportResults as &$value) { ?>
-            <tbody>
+              <tbody>
                 <?php // write engineerName each time engineer changes
-                  while($engineergroup != $value["idengineers"]) {
+                  while($engineergroup !== $value["idengineers"]) {
                     echo "<tr><td class=\"hdtitle listheader titlepadding\" colspan=\"6\"><h3>".$value["engineerName"] . " (".$outputCount[$value["engineerName"]] .")</h3></td></tr>";
                     $engineergroup = $value["idengineers"];
                   }?>
-              <tr>
-                <td class="hdtitle listheader" colspan="6"><a href="/ticket/view/<?php echo $value["callid"] ?>" alt="view ticket"><?php echo $value["title"] ?></a></td>
-              </tr>
-              <tr>
-                <td><span class="status<?php echo $value["status"] ?>"><?php echo $value["statusCode"] ?></span></td>
-                <td>#<?php echo $value["callid"] ?></td>
-                <td><?php echo date("d/m/Y", strtotime($value["opened"])) ?> - <?php echo $value["daysold"] ?> days</td>
-                <td><img src="/public/images/<?php echo $value["iconlocation"] ?>" width="19" height="20" alt="<?php echo $value["locationName"] ?>" title="<?php echo $value["locationName"] ?>"/></td>
-                <td><?php echo $value["engineerName"] ?></td>
-                <td><a href="/ticket/view/<?php echo $value["callid"] ?>" alt="view ticket"><img src="/public/images/ICONS-view.svg" width="24" height="25" class="icon" alt="view ticket" /></a></td>
-              </tr>
-            </tbody>
+                <tr>
+                  <td class="hdtitle listheader" colspan="6"><a href="/ticket/view/<?php echo $value["callid"] ?>" alt="view ticket"><?php echo $value["title"] ?></a></td>
+                </tr>
+                <tr>
+                  <td><span class="status<?php echo $value["status"] ?>"><?php echo $value["statusCode"] ?></span></td>
+                  <td>#<?php echo $value["callid"] ?></td>
+                  <td><?php echo date("d/m/Y", strtotime($value["opened"])) ?> - <?php echo $value["daysold"] ?> days</td>
+                  <td><img src="/public/images/<?php echo $value["iconlocation"] ?>" width="19" height="20" alt="<?php echo $value["locationName"] ?>" title="<?php echo $value["locationName"] ?>"/></td>
+                  <td><?php echo $value["engineerName"] ?></td>
+                  <td><a href="/ticket/view/<?php echo $value["callid"] ?>" alt="view ticket"><img src="/public/images/ICONS-view.svg" width="24" height="25" class="icon" alt="view ticket" /></a></td>
+                </tr>
+              </tbody>
             <?php }
+
             } ?>
         </table>
 
