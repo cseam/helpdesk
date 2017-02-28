d70cd3fe4a2506792ebe34ca42cc62243c801f0f
atomicjam <atomicjam@gmail.com>
2017-02-28 14:50:25 +0000

    added simple permission deny on ticket view

--- controllers/viewTicketController.php
+++ controllers/viewTicketController.php
@@ -14,7 +14,7 @@ class viewTicketController {
     $engineersModel = new engineerModel();
     //create empty object to store data for template
     $templateData = new stdClass();
-
+    $view = new Page();
     //pass details for template
     $templateData->ticketid = $ticketid;
     //populate users ticket list
@@ -24,18 +24,22 @@ class viewTicketController {
     $templateData->ticketUpdates = $ticketModel->getTicketUpdatesByCallId($ticketid);
     $templateData->additionalDetails = $ticketModel->getAdditionalDetails($ticketid);
     $templateData->ticketDetails["subscribed"] = $subscriptionModel->amISuscribedToTicket($_SESSION['engineerId'], $ticketid);
+    //check user has rights to view ticket engineers can see any ticket
+    if ($_SESSION['engineerLevel'] == '0' & $_SESSION['sAMAccountName'] != $templateData->ticketDetails["owner"]) {
+      $view->setTemplate('nopermissionView');
+      $view->setDataSrc(null);
+      $view->render();
+      exit;
+    }
     //populate call reasons for this tickets helpdeskid
     $templateData->callreasons = $callreasonsModel->getReasonsByHelpdeskId($templateData->ticketDetails["helpdesk"]);
     //populate quick responses
     $templateData->quickresponse = $quickresponseModel->getQuickResponseByHelpdeskId($templateData->ticketDetails["helpdesk"]);
     //get engineers for helpdesk
     $templateData->engineersList = $engineersModel->getListOfEngineersByHelpdeskId($templateData->ticketDetails["helpdesk"]);
-
     //update ticket views with user id and time stamp
     $ticketModel->logViewTicketById($ticketid, $_SESSION['sAMAccountName']);
-
     //pass complete data and template to view engine and render
-    $view = new Page();
     $view->setTemplate('ticketView');
     $view->setDataSrc($templateData);
     $view->render();
