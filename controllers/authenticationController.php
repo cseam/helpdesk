8a3f55ef85ce4123d10439f2e2a5e65e1e5d896f
atomicjam <atomicjam@gmail.com>
2017-03-01 11:13:48 +0000

    tidy up auth controller

--- controllers/authenticationController.php
+++ controllers/authenticationController.php
@@ -5,30 +5,33 @@ class authenticationController {
   {
     // start sessions
     session_start();
-    // check session populated
+    // check session populated if empty should forward to login in most cases
     if (empty($_SESSION['sAMAccountName'])) {
-      // session looks empty route to login page if not already there
         $baseurl = explode('/', $_SERVER['REQUEST_URI']);
-
-        if (!$_POST) {
-        // needs some work to stop post to pages other than login working but in principal posts to login need to not reroute
-
-          if (!preg_match("%login%", $baseurl[1])) {
-            if (preg_match("%digitalsign%", $baseurl[1])) { } else {
-              if (!preg_match("%logout%", $baseurl[1])) {
-                  // if not on the logout page set the entry point
-                    $noentrypoint = array("/favicon.ico", "/apple-touch-icon.png", "/apple-touch-icon-precomposed.png", "/login");
-                    if (in_array($_SERVER['REQUEST_URI'], $noentrypoint)) { $_SESSION['entrypoint'] = "/"; } else { $_SESSION['entrypoint'] = $_SERVER['REQUEST_URI']; }
-              }
-              // reroute
-              header('Location: /login/');
-              exit;
-            }
+        $reroute = 1; // set reroute to be default behaviour
+        // dont reroute login pages as your already there and we dont want an endless loop
+        if (preg_match("%login%", $baseurl[1])) {
+          $reroute = 0;
+        }
+        // dont reroute digitalsign as they open to public
+        if (preg_match("%digitalsign%", $baseurl[1])) {
+          $reroute = 0;
+        }
+        // reroute assuming a match hasnt been found
+        if ($reroute == 1) {
+          header('Location: /login/');
+          exit;
+        }
+        // if not on the logout page set the entry point
+        if (!preg_match("%logout%", $baseurl[1])) {
+          $noentrypoint = array("/favicon.ico", "/apple-touch-icon.png", "/apple-touch-icon-precomposed.png", "/login");
+          $_SESSION['entrypoint'] = $_SERVER['REQUEST_URI'];
+          if (in_array($_SERVER['REQUEST_URI'], $noentrypoint)) {
+            $_SESSION['entrypoint'] = "/";
           }
         }
     }
   }
 }
-
 // check users session is populated else reroute to login.
 new authenticationController();
